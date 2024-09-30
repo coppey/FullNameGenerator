762ec2a91bd5ca79608bc17c9886e1c9b93e125a
unknown <you@example.com>
2024-09-30 15:14:32 -0500

    method created

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -10,11 +10,27 @@ public class Main
         /*
         prompt user to enter name into different variables to be called by method below
          */
-        System.out.println("Hello world!");
+        System.out.println("Please enter your first name: ");
+        String firstName = sc.nextLine();
+        System.out.println("Please enter your middle name: ");
+        String middleName = sc.nextLine();
+        System.out.println("Please enter your last name: ");
+        String lastName = sc.nextLine();
+
+        System.out.println(fullName(firstName, middleName, lastName));
+
     }
 
     /*
     FullName method will be used to put together a full name
     will add variables in order to complete single variable returned by method
      */
+
+    public static String fullName(String firstName, String middleName, String lastName)
+    {
+        String name = firstName + " " + middleName + " " + lastName;
+        name = name.trim();
+
+        return name;
+    }
 }
