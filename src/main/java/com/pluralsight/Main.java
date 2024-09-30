ac0f5a7df511c1d24945289fddd0dac3e22c9d16
unknown <you@example.com>
2024-09-30 15:05:14 -0500

    first commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        /*
+        prompt user to enter name into different variables to be called by method below
+         */
+        System.out.println("Hello world!");
+    }
+
+    /*
+    FullName method will be used to put together a full name
+    will add variables in order to complete single variable returned by method
+     */
+}
