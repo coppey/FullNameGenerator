0387a97ff0c4c4dd44299de342e9fbcc649d94a0
unknown <you@example.com>
2024-10-01 09:17:20 -0500

    instructor best practice version

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,21 +3,47 @@ import java.util.*;
 
 public class Main
 {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+
+
+
+        //Scanner sc = new Scanner(System.in);
 
         /*
         prompt user to enter name into different variables to be called by method below
          */
-        System.out.println("Please enter your first name: ");
-        String firstName = sc.nextLine();
-        System.out.println("Please enter your middle name: ");
-        String middleName = sc.nextLine();
-        System.out.println("Please enter your last name: ");
-        String lastName = sc.nextLine();
+        System.out.print("Please enter your first name: ");
+        String firstName = sc.nextLine().trim();
 
-        System.out.println(fullName(firstName, middleName, lastName));
+        System.out.print("Please enter your middle name: ");
+        String middleName = sc.nextLine().trim();
+
+        System.out.print("Please enter your last name: ");
+        String lastName = sc.nextLine().trim();
+
+        System.out.print("Please enter your Suffix: ");
+        String suffix = sc.nextLine().trim();
+
+        //firstname = firstName.indexOf(0).toUpperCase();
+
+        if(!middleName.equals("") && !suffix.equals(""))
+        {
+            System.out.println("Your full name is: " + fullName(firstName,"", lastName,""));
+        }else
+        {
+            System.out.println("Your full name is: " + fullName(firstName, middleName, lastName, suffix));
+
+        }
+
+
+
+
+
+        //System.out.println(fullName(firstName, middleName, lastName, suffix));
 
     }
 
@@ -26,9 +52,9 @@ public class Main
     will add variables in order to complete single variable returned by method
      */
 
-    public static String fullName(String firstName, String middleName, String lastName)
+    public static String fullName(String firstName, String middleName, String lastName, String suffix)
     {
-        String name = firstName + " " + middleName + " " + lastName;
+        String name = firstName + " " + middleName + " " + lastName + " " + suffix;
         name = name.trim();
 
         return name;
